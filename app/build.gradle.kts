73191492051fe16058d9a68b1d2b8d8d0a7acdda
gpd114 <gpd114@googlemail.com>
2026-08-09 07:13:04 +0100

    Added icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.debritsu.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 6
-        versionName = "0.4.2"
+        versionCode = 7
+        versionName = "0.5.0"
     }
 
     signingConfigs {
